f4b492005f198eb90ac1917e9232c64c6045f9c9
dianlujitao <dianlujitao@lineageos.org>
2019-09-06 11:58:28 +0800

    audio: Use generated kernel headers
    Change-Id: Ie7911305a0d06cb7a204ed78fcab058ad972ee7f
    Signed-off-by: klozz <klozz@vivaldi.net>

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -56,7 +56,8 @@ endif
 
 LOCAL_HEADER_LIBRARIES := libhardware_headers \
                           libsystem_headers \
-                          libutils_headers
+                          libutils_headers \
+                          generated_kernel_headers
 
 LOCAL_SHARED_LIBRARIES := \
         libcutils \
@@ -70,23 +71,14 @@ LOCAL_MODULE_RELATIVE_PATH := soundfx
 LOCAL_MODULE:= libqcompostprocbundle
 LOCAL_VENDOR_MODULE := true
 
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
 LOCAL_C_INCLUDES := \
         external/tinyalsa/include \
-        $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include \
         $(call include-path-for, audio-effects)
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DLKM)),true)
   LOCAL_HEADER_LIBRARIES += audio_kernel_headers
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
-endif
-
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
-        LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
 
 include $(BUILD_SHARED_LIBRARY)
@@ -161,7 +153,8 @@ LOCAL_CFLAGS+= -O2 -fvisibility=hidden
 
 LOCAL_HEADER_LIBRARIES := libhardware_headers \
                           libsystem_headers \
-                          libutils_headers
+                          libutils_headers \
+                          generated_kernel_headers
 
 LOCAL_SHARED_LIBRARIES := \
         libcutils \
@@ -172,12 +165,9 @@ LOCAL_MODULE_RELATIVE_PATH := soundfx
 LOCAL_MODULE:= libvolumelistener
 LOCAL_VENDOR_MODULE := true
 
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
 LOCAL_C_INCLUDES := \
         hardware/qcom/audio/hal \
-        $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-        $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include \
+        $(call project-path-for,qcom-audio)/hal \
         external/tinyalsa/include \
         $(call include-path-for, audio-effects) \
         $(call include-path-for, audio-route) \
@@ -188,11 +178,6 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DLKM)),true)
   LOCAL_HEADER_LIBRARIES += audio_kernel_headers
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
-endif
-
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
-        LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
 
 include $(BUILD_SHARED_LIBRARY)
